c4ccf6575f893f5e89791ea6632600609b1a6e31
mogakaowen <owenmogaka@gmail.com>
2022-06-27 18:14:25 +0300

    Nested for loops

---
+++ nested-loops.py
@@ -0,0 +1,13 @@
+for i in range(2):
+    #For each iteration of the i loop above the j loop below is executed
+    for j in range(4):
+        sum=i+j
+        print(f"{i} + {j}= {sum}")
+
+print()
+
+for i in range(2):
+    #For each iteration of the i loop above the j loop below is executed
+    for j in range(4):
+        print(j, end=" ")
+    print()
